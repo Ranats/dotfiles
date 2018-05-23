476c349505de9cdaebdbdbd97731735475b86362
Ranats <sopurani885@gmail.com>
2018-05-23 18:51:56 +0900

    add .zprofile

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 ln -sf ~/dotfiles/.moc/config ~/.moc/config
 ln -sf ~/dotfiles/.moc/keymap ~/.moc/keymap
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/bin/mstsc ~/bin/mstsc
