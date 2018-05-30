409b79a7452ad409f8173d72f753e9934b00a806
Ranats <sopurani885@gmail.com>
2018-05-30 18:26:28 +0900

    add xmonad

--- install.sh
+++ install.sh
@@ -4,3 +4,8 @@ ln -sf ~/dotfiles/.moc/keymap ~/.moc/keymap
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/bin/mstsc ~/bin/mstsc
+ln -sf ~/dotfiles/.xmonad/xmobarrc ~/.xmonad/xmobarrc
+ln -sf ~/dotfiles/.xmonad/xmonad.hi ~/.xmonad/xmonad.hi
+ln -sf ~/dotfiles/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+ln -sf ~/dotfiles/.xmonad/xmonad.o ~/.xmonad/xmonad.o
+ln -sf ~/dotfiles/.xmonad/xmonad-x86_64-linux ~/.xmonad/xmonad-x86_64-linux
