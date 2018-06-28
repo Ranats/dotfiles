c8bad8c0c0d4ec5902cef5adcfa3b35b17037860
Ranats <sopurani885@gmail.com>
2018-06-28 23:58:20 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,52 @@
 #!/bin/sh
-if [[ `uname -r` =~ ARCH$ ]]; then
+
+while getopts S: OPT
+do
+  case $OPT in
+    S) FLG_S="TRUE" ;;
+    *) echo "Usage: ./install.sh [-S]" 1>&2
+       exit 1 ;;
+  esac
+done
+
+
+# TD: fix
+
+if ["$FLG_S" = "TRUE"]; then
+  if [[ `uname -r` =~ ARCH$ ]]; then
+    yaourt -S moc --noconfirm
+    yaourt -S vim --noconfirm
+    yaourt -S zsh --noconfirm
+    yaourt -S freerdp --noconfirm
+    yaourt -S xmonad xmonad-contrib --noconfirm
+    yaourt -S imagemagick --noconfirm
+    yaourt -S rxvt-unicode --noconfirm
+    yaourt -S ranger --noconfirm
+  # dmenu: dynamic menu
+    yaourt -S dmenu --noconfirm
+  else
+    echo "You are not Arched!!!"    
+  fi
+fi
+
 # Music On Console: music player on console
   rm -rf ~/.moc/
-  yaourt -S moc --noconfirm
   ln -sf ~/dotfiles/.moc/config ~/.moc/config
   ln -sf ~/dotfiles/.moc/keymap ~/.moc/keymap
 
 # Vim
-  yaourt -S vim --noconfirm
   rm ~/.vimrc
   ln -sf ~/dotfiles/.vimrc ~/.vimrc
 
 # Zsh
-  yaourt -S zsh --noconfirm
   rm ~/.zshprofile
   ln -sf ~/dotfiles/.zprofile ~/.zprofile
   
 # xfreeRDP : remote desktop to Windows
-  yaourt -S freerdp --noconfirm
   ln -sf ~/dotfiles/bin/mstsc ~/bin/mstsc
 
 # Xmonad
   rm -rf ~/.xmonad/
-  yaourt -S xmonad xmonad-contrib --noconfirm
   ln -sf ~/dotfiles/.xmonad/xmobarrc ~/.xmonad/xmobarrc
   ln -sf ~/dotfiles/.xmonad/xmonad.hi ~/.xmonad/xmonad.hi
   ln -sf ~/dotfiles/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
@@ -31,18 +55,15 @@ if [[ `uname -r` =~ ARCH$ ]]; then
   ln -sf ~/dotfiles/.xinitrc ~/.xinitrc
 
 # Screenshot: use ImageMagic
-  yaourt -S imagemagick --noconfirm
   ln -sf ~/dotfiles/bin/screenshot.sh ~/bin/screenshot.sh
   ln -sf ~/dotfiles/bin/screenshot_select.sh ~/bin/screenshot_select.sh
 
 # urxvt: Awesome terminal
-  yaourt -S rxvt-unicode --noconfirm
   ln -sf ~/dotfiles/bin/run_urxvt ~/bin/run_urxvt
   ln -sf ~/dotfiles/.Xresources ~/.Xresources
   xrdb -merge ~/.Xresources
  
 # ranger: filemanager on console
-  yaourt -S ranger --noconfirm
   ln -sf ~/dotfiles/bin/run_ranger ~/bin/run_ranger
   ranger --copy-config=all
   rm ~/.config/ranger/rc.conf
@@ -51,10 +72,3 @@ if [[ `uname -r` =~ ARCH$ ]]; then
   ln -sf ~/dotfiles/.config/ranger/rc.conf ~/.config/ranger/rc.conf
   ln -sf ~/dotfiles/.config/ranger/rifle.conf ~/.config/ranger/rifle.conf
   ln -sf ~/dotfiles/.config/ranger/scope.sh ~/.cofig/ranger/scope.sh
-
-# dmenu: dynamic menu
-  yaourt -S dmenu --noconfirm
-
-else
-  echo "You are not Arched!!!"
-fi
